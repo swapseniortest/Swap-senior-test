a09e0ecbeb6e2c2d4a38d02ec6317f39d54f9b6f
swapseniortest <swapseniortest@gmail.com>
2024-10-08 20:23:16 -0300

    Primeiro commit do micro-serviço de recuperação de issues no github

---
+++ src/main/java/com/swap/issues/recovery/service/IssuesRecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.swap.issues.recovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesRecoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swap/issues/recovery/service/IssuesRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swap.issues.recovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
